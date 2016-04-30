b359f6aa7fc8aadd1fd33a19df3b7fbb4a48d56e
jwilliams <jeff.williams@contrastsecurity.com>
2016-04-30 16:08:16 -0400

    asdfasdfasdfasf

--- lib/java_buildpack/framework/contrast_agent.rb
+++ lib/java_buildpack/framework/contrast_agent.rb
@@ -24,22 +24,6 @@ module JavaBuildpack
     # Encapsulates the functionality for enabling zero-touch Contrast support.
     class ContrastAgent < JavaBuildpack::Component::BaseComponent
 
-      # (see JavaBuildpack::Component::BaseComponent#detect)
-      def detect
-          puts "contrast-agent.jar=3.2.7"
-		  exit 0
-	  end
-
-      # (see JavaBuildpack::Component::BaseComponent#compile)
-      def compile
-        @droplet.copy_resources
-      end
-
-      # (see JavaBuildpack::Component::BaseComponent#release)
-      def release
-        @droplet.java_opts
-      end
-
     end
   end
 end
