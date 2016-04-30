cfdad7d49683279b390513bc0fe92f6525f519bd
jwilliams <jeff.williams@contrastsecurity.com>
2016-04-30 13:20:59 -0400

    asdfasdfasfd

--- lib/java_buildpack/framework/contrast_agent.rb
+++ lib/java_buildpack/framework/contrast_agent.rb
@@ -44,7 +44,7 @@ module JavaBuildpack
       def detect
 		agent_path = File.join ARGV[0], "WEB-INF/lib/contrast.jar"
 		if File.exist?(agent_path)
-		  puts "#{self.class.to_s.dash_case}=3.2.7"
+		  puts agent_path +"=3.2.7"
 		  exit 0
 		else
 		  exit 1
