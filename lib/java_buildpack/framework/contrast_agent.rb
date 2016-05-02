604d3ee8223f6ec50e0bcca1ff1df246a94816fa
jwilliams <jeff.williams@contrastsecurity.com>
2016-05-01 21:51:40 -0400

    asfdasf

--- lib/java_buildpack/framework/contrast_agent.rb
+++ lib/java_buildpack/framework/contrast_agent.rb
@@ -35,7 +35,7 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        @droplet.java_opts.add_javaagent( @application.root +"/WEB-INF/contrast.jar")
+        @droplet.java_opts.add_javaagent( "/WEB-INF/contrast.jar")
       end
 
     end
